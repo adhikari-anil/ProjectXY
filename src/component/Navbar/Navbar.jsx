ac30931f262c61820091ef69dc672651bbdcd59d
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-17 23:16:36 +0545

    A simple Navbar is added for layout check..

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavBarItems } from "../../constant/MenuItem";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex pt-6 pb-6 pl-20 pr-20 justify-between">
+      <div className="flex justify-center items-center">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBlLLxIkE9p5IniBVIupwaILQg4Ozd90Z6KA&s"
+          alt="WEBY"
+          className="h-20 w-40"
+        />
+      </div>
+      <div className="flex justify-center items-center gap-4">
+        {NavBarItems.map((item, index) => (
+          <Link to={item.path} key={index}>
+            <div className="flex justify-center items-center rounded-sm hover:bg-slate-800 hover:text-yellow-50 font-thin text-lg w-40 p-2">
+              <p>{item.name}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="flex justify-center items-center h-20 w-40 border-2 rounded-lg">
+        <button>Contact Button</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
