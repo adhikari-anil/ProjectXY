80f51868201c1ee9e269ec827a085a5f34175ba0
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-17 23:11:09 +0545

    Project is setup and Basis requirement for Navbar is also added..

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Project is setup and Basic requirement for Navbar is also added.</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/constant/MenuItem.js
@@ -0,0 +1,22 @@
+export const NavBarItems = [
+  {
+    name: "Home",
+    path: "/home",
+  },
+  {
+    name: "Service",
+    path: "/service/",
+  },
+  {
+    name: "Pricing",
+    path: "/price/",
+  },
+  {
+    name: "Our Works",
+    path: "/work/",
+  },
+  {
+    name: "Blogs",
+    path: "/blogs/",
+  },
+];
