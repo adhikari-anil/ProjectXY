cbd59b3afd5b21c0f6aef73e8ce73008189581d2
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-22 21:04:12 +0545

    LandingPage added and little changes in main.jsx fille.

--- src/component/LandingPage/LandingPage.jsx
+++ src/component/LandingPage/LandingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+  let navigate = useNavigate();
+
+  const handleClick = ()=>{
+    navigate("/home");
+  };
   return (
-    <div className='flex justify-center items-center'>
+    <div className='h-screen w-screen flex justify-center items-center'>
         <div className='flex gap-5'>
-            <button>Yes</button>
+            <button onClick={handleClick}>Yes</button>
             <button>No</button>
         </div>
     </div>
