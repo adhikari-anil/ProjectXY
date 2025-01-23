3491ae59d73cd10fa8a65cbd61543ca7878e53c2
Anil Adhikari <adhikarianil719@gmail.com>
2025-01-23 09:35:30 +0545

    New Route is created for main landing page.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,11 +5,13 @@ import {
 } from "react-router-dom";
 import LandingPage from "./component/LandingPage/LandingPage";
 import App from "./App";
+import MainLandingPage from "./component/LandingPage/MainLandingPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" element={<LandingPage />} />
+      <Route path="/" element={<MainLandingPage />} />
+      <Route path="/start" element={<LandingPage />} />
       <Route path="/home" element={<App />} />
     </>
   )
